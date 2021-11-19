648db961ad94dd4547170f5b9a7dff9cbbf647a2
ionicr1 <20784324+ionicr1@users.noreply.github.com>
2021-11-19 16:49:37 -0500

    Create Carrots.py

---
+++ Carrots.py
@@ -0,0 +1,9 @@
+list = []
+wordList = []
+for i in range(0,2):
+    issues = int(input())
+    list.append(issues)
+for i in range(0,2):
+    word = input()
+    wordList.append(word)
+print(list[1])
